0270e0bb421eee6d4b68bdd487687bf5dc5a6dff
Klim <Severin.Klim@yandex.by>
2021-06-22 15:16:53 +0300

    Add project files.

---
+++ Company/Salary.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Salary] (
+    [Id]         INT   IDENTITY (1, 1) NOT NULL,
+    [EmployeeId] INT   NULL,
+    [Month]      INT   NULL,
+    [Year]       INT   NULL,
+    [Amount]     MONEY NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Salary_ToEmployee] FOREIGN KEY ([EmployeeId]) REFERENCES [dbo].[Employee] ([Id])
+);
+
